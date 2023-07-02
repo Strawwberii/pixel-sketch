7194e3ebd7739031d69b98a5b328c66429ecff0c
Rupok ghosh <rupokghosh291@gmail.com>
2023-07-01 20:03:45 -0700

    add css grid to create sketchpad

--- script.js
+++ script.js
@@ -4,16 +4,23 @@ let sketchpad = document.getElementById("sketchpad");
 
 // create sketchpad grid
 
+sketchpad.style.gridTemplateColumns = 'repeat(16, 1fr)';
+sketchpad.style.gridTemplateRows = 'repeat(16, 1fr)';
+
 function sketchGrid(noOfGrids){
 
     for(i=0;i<noOfGrids;i++){
 
         const grids = document.createElement("div");
         grids.className  = 'grids';
-        grids.innerText = 'div'
+        grids.innerText = 'a';
         sketchpad.append(grids);
     }
     
 }
 
 sketchGrid(256);
+
+sketchpad.addEventListener('mouseover', function(){
+    
+})

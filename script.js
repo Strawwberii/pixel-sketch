5f5f4c0a63c0ff65481b04542756f429a78bf72a
Rupok ghosh <rupokghosh291@gmail.com>
2023-07-04 20:30:01 -0700

    original and eraser btn functions

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //global variables
 
 let sketchpad = document.getElementById("sketchpad");
+let clearBtn = document.getElementById("clearbtn");
 let slider = document.getElementById("slider");
 let value = slider.value;
 let sliderValue = document.getElementById('sliderValue');
+let color = 'black';
+let colorChoose = document.getElementById('colorChoose');
+let original = document.getElementById('original');
+let rainbowBtn = document.getElementById('rainbowbtn');
+let eraserBtn = document.getElementById('eraserbtn');
+
 
 
 // create sketchpad grid
@@ -12,29 +19,42 @@ function sketchGrid(value){
 
     sketchpad.style.gridTemplateColumns = `repeat(${value}, 1fr)`;
     sketchpad.style.gridTemplateRows = `repeat(${value}, 1fr)`;
-
+    let gridSquares = sketchpad.querySelectorAll('div');
+    gridSquares.forEach((div) => div.remove());
     for(i=0;i<(value*value);i++){
 
         let grids = document.createElement("div");
         grids.className  = 'grids';
-        grids.innerText = 'div';
         sketchpad.append(grids);
-        
+
         // change color on hover
+
         grids.addEventListener('mouseover', function(){
-            grids.style.backgroundColor = 'blue';
+            grids.style.backgroundColor = color;
         });
-    }
-    
-    
+    } 
+    console.log(value);
 }
 
-// change grid's size
+// change grid's size (by taking input from slider)
 
 function changeGridSize(input){
     sketchGrid(input);
 };
-changeGridSize(input);
 
-// show sliderValue on screen
+// function to change color of mouseover
 
+function changeColor(colorChoice){
+    color = colorChoice;
+}
+
+// adding changeColor function to all the buttons
+
+original.addEventListener('click', function(){
+    changeColor('black');
+});
+
+
+eraserBtn.addEventListener('click', function(){
+    changeColor('wheat');
+});
